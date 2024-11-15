20c5fe92a2811598f0d5a8484a59a72bad250afb
aswm0121 <143142634+aswm0121@users.noreply.github.com>
2024-11-15 22:41:28 +0900

    Add files via upload

---
+++ 관리자_p/practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    cout << "중앙대학교 경영학과 성적관리 프로그램에 오신 걸 환영합니다." << endl;
+    cout << "아이디(학번)을 입력하주세요: " << endl;
+    cout << "이름을 입력해 주세요: " << endl;
+    cout << "프로그램을 종료합니다. \n감사합니다." << endl;
+}
